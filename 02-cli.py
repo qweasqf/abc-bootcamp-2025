cbf5601af4b7ae396e3734c148e767063ea5b9f4
qweasqf <bhmin0615@naver.com>
2025-08-02 19:06:36 +0900

    @지난주 실습내역 커밋

---
+++ 02-cli.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv() #env 파일에서 환경변수 로드
+
+import os
+API_KEY = os.environ["OPENAI_API_KEY"]
+
+from openai import OpenAI
+client = OpenAI(api_key = API_KEY)  # OPENAI_API_KEY 환경변수 지정이 필요
+
+response = client.responses.create(
+    model="gpt-4o",
+    # input="Write a one-sentence bedtime story about a unicorn in korean."
+    input = "make python code for factorial",
+)
+
+print("usage :", response.usage)
+print(response.output_text)
+
